8981fe1d31481c469f2c8766c2790d898e093b04
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-02-05 14:23:19 +0100

    Making the amiibo database into a global variable, for faster load times

--- src/composables/amiibos.ts
+++ src/composables/amiibos.ts
@@ -1,18 +1,23 @@
-// import { AmiiboContent } from "@/types/Amiibo";
-
-export async function getAllAmiibos() {
+export async function fetchAllAmiibos() {
     const res = await fetch(`https://www.amiiboapi.com/api/amiibo`);
     const json = await res.json() as {amiibo: AmiiboType[]};
     return json.amiibo;
 }
-
-export async function getOneAmiibo(id: string) {
+export async function fetchOneAmiibo(id: string) {
     const res = await fetch(`https://www.amiiboapi.com/api/amiibo?id=${id}`);
     const json = await res.json() as {amiibo: AmiiboType};
     return json.amiibo;
 }
 
+const AMIIBO_DATABASE = fetchAllAmiibos();
 
+export async function getAllAmiibos() {
+    return await AMIIBO_DATABASE;
+}
+
+export async function getOneAmiibo(id: string) {
+    return (await AMIIBO_DATABASE).find(amiibo => id === `${amiibo.head}${amiibo.tail}`);
+}
 
 
 export type AmiiboType = {

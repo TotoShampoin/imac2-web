13f7c74ee9d7dcbdd0d7ed9c099659104a9ace86
TotoShampoin <thomas.garnier@totoshampoin.fr>
2024-02-26 15:49:08 +0100

    Yes, this loads

--- src/components/PageView.vue
+++ src/components/PageView.vue
@@ -3,13 +3,17 @@ import { getOneAmiibo, AmiiboType } from '@/composables/amiibos';
 import { ref } from 'vue';
 import { useRoute } from 'vue-router';
 import AmiiboContent from './AmiiboContent.vue';
+import LoadIcon from './LoadIcon.vue';
 
 const route = useRoute();
 const id = route.params.id as string;
+
 const amiibo = ref<AmiiboType>();
+const loading = ref<boolean>(false);
 
 async function loadAmiibo() {
     amiibo.value = await getOneAmiibo(id);
+    loading.value = true;
 }
 loadAmiibo();
 
@@ -19,6 +23,7 @@ loadAmiibo();
     <section>
         <AmiiboContent :amiibo="amiibo" />
     </section>
+    <LoadIcon :shown="!loading" />
 </template>
 
 <style lang="scss" scoped>
